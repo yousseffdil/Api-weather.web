143152a04e1c47fa087ed3b3121dc50fe146c408
yousseffdil <fdilyoussef@gmail.com>
2023-03-23 13:58:42 +0100

    v4

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+
 // Crearem les constats que ens ajudaran mes endevant 
 const searchInput = document.querySelector('.search input');
 const cityName = document.createElement('h1');
@@ -14,13 +15,11 @@ icon.classList.add('icon');
 // Aquest addeventListener es el que ens ajudar a buscar tot, basicament es un addEventListener amb un IF el cual s'encarrega de mapajar l'enter, en cas de clicar
 // Enter fara tot lo que estigui dins
 searchInput.addEventListener('keyup', (event) => {
-    if (event.keyCode === 13) {
+    if (event.keyCode === 13) { 
         // API-KEY 
         const apiKey = 'aba6ff9d6de967d5eac6fd79114693cc';
         // Agafem el que escribim dins del input com a ciudad
-
         const city = searchInput.value;
-
         // En aquest fetch basicament el que es fa es agafar el temps d'avull
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
             .then(response => response.json())
@@ -30,56 +29,56 @@ searchInput.addEventListener('keyup', (event) => {
                 const temp = Math.round(data.main.temp);
                 Info.innerHTML = `<h1 class="tmp">${temp} °C<h1>`;
                 const iconCode = data.weather[0].icon;
-                const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+                const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
                 icon.setAttribute('src', iconUrl);
                 Info.appendChild(cityName);
                 Info.appendChild(icon);
             })
             .catch(error => console.log(error) || alert("Ingresa una ciudad o pueblo valido"));
-
         // Aquest fetch s'encarrega de buscar i mostrar la previsio de cada 3h i la previsio dels 5 dias de la semana.
         fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`)
             .then(response => response.json())
             .then(data => {
                 // Reiniciem el divisor per cada busqueda que es fagui per poder afaguir mes coses
                 botHours.innerHTML = "";
-                const forecasts = data.list.filter((_forecast, index) => index % 8 === 0);
+                //const forecasts = data.list.filter((_forecast, index) => index % 8 === 0);
+                const forecasts = data.list;
+
+                let fecha = "";
+
                 // Bloc de codi que ens ajuda a mostrar-ho tot dins del divisor
                 forecasts.forEach(forecast => {
                     const temp = Math.round(forecast.main.temp);
                     const iconCode = forecast.weather[0].icon;
                     const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-                    const timestamp = new Date(forecast.dt * 900).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+                    const timestamp = new Date(forecast.dt * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+                    const diaAnterior = fecha;
+                    fecha = new Date(forecast.dt_txt).toLocaleDateString([]);
+
                     const forecastElement = document.createElement('div');
                     forecastElement.classList.add('time-hour');
+
                     forecastElement.innerHTML = `
                         <h3 class="time">${timestamp}</h3>
                         <img class="icon" src="${iconUrl}" alt="${forecast.weather[0].description}">
                         <h3 class="temp">${temp} °C</h3>
                     `;
+
+
+                    if (diaAnterior != fecha) {
+                        botHours.innerHTML += `
+                        <h2 class= fecha> ${fecha}</h2>
+                        `;
+                    }
+
                     botHours.appendChild(forecastElement);
                 });
 
                 // Reiniciem el divisor
-                slideRight.innerHTML = "";
-                const dailyForecasts = data.list.filter((_forecast, index) => index % 8 === 0);
+                const dailyForecasts = data.list;
                 // Bloc de codi on s'hafagueix tot lo de la semana
                 dailyForecasts.forEach(forecast => {
-                    const weekday = new Date(forecast.dt).toLocaleDateString([], { weekday: 'long' });
-                    const tempMin = Math.round(forecast.main.temp_min);
-                    const tempMax = Math.round(forecast.main.temp_max);
-                    const iconCode = forecast.weather[0].icon;
-                    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-                    const description = forecast.weather[0].description;
-
-                    const forecastElement = document.createElement('div');
-                    forecastElement.classList.add('Prevision-semana');
-                    forecastElement.innerHTML = `
-            <h3 class="weekday">${weekday}</h3>
-            <img class="icon" src="${iconUrl}" alt="${description}">
-            <h3 class="temp"><span class="temp-min">${tempMin} °C</span>/<span class="temp-max">${tempMax} °C</span></h3>
-          `;
-                    slideRight.appendChild(forecastElement);
                     // Fem un callback per cridar la funcio del grafic
                     const humidityData = data.list.filter((_forecast, index) => index % 8 === 0);
                     showHumidityChart(humidityData);
@@ -105,7 +104,9 @@ searchInput.addEventListener('keyup', (event) => {
                     pointHoverBackgroundColor: '#4c6ef5',
                     pointHoverBorderColor: '#4c6ef5'
                 }]
+
             };
+
 
             const chartConfig = {
                 type: 'line',
